0fd8bf374323eced01a69ebe9257266993cce687
jessiejessiewang <jessiewang0907@gmail.com>
2022-02-20 18:06:38 +0800

    ENH: change linear model to LSTM

--- cmr/returns.py
+++ cmr/returns.py
@@ -1,18 +1,17 @@
-import logging
-
-import cvxportfolio as cvx
 import pandas as pd
-import sklearn.impute as impute
-import sklearn.linear_model as linear_model
-import sklearn.metrics as metrics
-import sklearn.preprocessing as pre
+import tensorflow as tf
+from dateutil.relativedelta import relativedelta
+from keras.layers import Dense, LSTM
+from keras.models import Sequential
 from lazy import lazy
-from sklearn.pipeline import make_pipeline
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.preprocessing import timeseries_dataset_from_array  # noqa
 
 from .data_loader import load_features
 
 
-class TaReturnsForecast(cvx.ReturnsForecast):
+class TaReturnsForecast(object):
 
     def __init__(self, symbols: list, start: pd.Timestamp, end: pd.Timestamp, resample_rule: str = '1H') -> None:
         """
@@ -27,12 +26,12 @@ class TaReturnsForecast(cvx.ReturnsForecast):
         self.start = start
         self.end = end
         self.resample_rule = resample_rule
-        self.fit_model()
-        super().__init__(self.get_prediction())
 
     @lazy
     def alpha_source(self):
-        return load_features(self.symbols, self.start, self.end, self.resample_rule)
+        feat = load_features(self.symbols, self.start, self.end, self.resample_rule)
+        feat = feat.reorder_levels(['time', 'symbol']).sort_index()
+        return feat
 
     @lazy
     def test_start(self):
@@ -43,55 +42,111 @@ class TaReturnsForecast(cvx.ReturnsForecast):
         return ((self.end - self.start) * .95 + self.start).round('D')
 
     @lazy
-    def model(self):
-        return make_pipeline(
-            impute.SimpleImputer(),
-            pre.RobustScaler(with_centering=False),
-            linear_model.LinearRegression()
+    def window(self):
+        return 3
+
+    @lazy
+    def raw_train_test(self):
+        feat = self.alpha_source
+        return (
+            feat.loc[feat.index.get_level_values(0) < self.test_start],
+            feat.loc[(feat.index.get_level_values(0) >= self.test_start - relativedelta(days=self.window))]
         )
 
+    @lazy
+    def is_pca(self):
+        return False
+
+    @lazy
+    def scaled_train_test(self):
+        raw_train, raw_test = self.raw_train_test
+
+        # standard
+        standard_scaler = StandardScaler()
+        scaled_train = pd.DataFrame(index=raw_train.index, data=standard_scaler.fit_transform(raw_train.values),
+                                    columns=raw_train.columns)
+        scaled_test = pd.DataFrame(index=raw_test.index, data=standard_scaler.transform(raw_test.values),
+                                   columns=raw_test.columns)
+        scaled_train['ret'] = raw_train['ret']
+        scaled_test['ret'] = raw_test['ret']
+
+        # symbol one hot encoder
+        dummy_train = pd.get_dummies(scaled_train.index.get_level_values('symbol'))
+        dummy_train.index = scaled_train.index
+        train = pd.concat([scaled_train, dummy_train], axis=1)
+
+        dummy_test = pd.get_dummies(scaled_test.index.get_level_values('symbol'))
+        dummy_test.index = scaled_test.index
+        test = pd.concat([scaled_test, dummy_test], axis=1)
+
+        # features PCA, from 85 features (to 21 features)
+        if self.is_pca:
+            pca_scaler = PCA(n_components=0.90)
+            train = pd.DataFrame(index=train.index, data=pca_scaler.fit_transform(train))
+            test = pd.DataFrame(index=test.index, data=pca_scaler.transform(test))
+
+        return train, test
+
+    @lazy
+    def generator_train_test(self):
+        raw_train, raw_test = self.raw_train_test
+        train, test = self.scaled_train_test
+
+        # number_of_features = train.shape[1]
+        train_index = pd.DataFrame()
+        test_index = pd.DataFrame()
+
+        for idx, symbol in enumerate(self.symbols):
+
+            x_train = train.loc[(slice(None), symbol),]
+            y_train = raw_train.loc[(slice(None), symbol), ['ret']][self.window:]
+
+            x_test = test.loc[(slice(None), symbol),]
+            y_test = raw_test.loc[(slice(None), symbol), ['ret']][self.window:]
+
+            _generator_train = timeseries_dataset_from_array(x_train, y_train, sequence_length=self.window,
+                                                             batch_size=None)
+            _generator_test = timeseries_dataset_from_array(x_test, y_test, sequence_length=self.window,
+                                                            batch_size=None)
+            if idx == 0:
+                generator_train = _generator_train
+                generator_test = _generator_test
+            else:
+                generator_train = generator_train.concatenate(_generator_train)
+                generator_test = generator_test.concatenate(_generator_test)
+            train_index = train_index.append(y_train)
+            test_index = test_index.append(y_test)
+
+        generator_train = generator_train.batch(128)
+        generator_test = generator_test.batch(128)
+
+        return generator_train, generator_test, train_index, test_index
+
+    @lazy
+    def model(self):
+        model = Sequential()
+        model.add(
+            LSTM(64, return_sequences=True, input_shape=(self.window, self.scaled_train_test[0].shape[1]),
+                 activation="tanh", dropout=0.2))
+        model.add(LSTM(64, return_sequences=False, activation="tanh"))
+        model.add(Dense(32, activation="tanh"))
+        model.add(Dense(1, activation="linear"))
+
+        # Compile the model
+        model.compile(optimizer='adam', loss='mean_absolute_error', metrics=['MSE', 'MAE'])
+        return model
+
     def fit_model(self):
-        logging.info("built signals from %s to %s" % (self.start, self.end))
+        # Train the model
+        callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=2, restore_best_weights=True)
+        self.model.fit(self.generator_train_test[0], epochs=7, callbacks=[callback])
+        # self.model.save('universe_%sh_7epoch_tanhDualLSTM.keras' % self.window)
 
-        # shift y
-        af = self.alpha_source.copy()
-        af['ret1d'] = af.groupby('symbol')['ret'].shift(-1).fillna(0)
+        # Predict
+        train_result = self.generator_train_test[2].copy()
+        train_result['y_train'] = self.model.predict(self.generator_train_test[0])
 
-        # split train and test
-        x = af.drop(columns=['ret1d'])
-        y = af['ret1d']
-        x_train = x  # x.loc[(x.index.get_level_values(0) < self.test_start)]
-        y_train = y  # y.loc[(y.index.get_level_values(0) < self.test_start)]
-        self.model.fit(x_train, y_train)
+        test_result = self.generator_train_test[3].copy()
+        test_result['y_pred'] = self.model.predict(self.generator_train_test[1])
 
-        # print out in-sample evaluation
-        y_pred = self.model.predict(x_train)
-        print("Mean absolute error (in-sample): %.6f" % metrics.mean_absolute_error(y_train, y_pred))
-        print("Mean squared error (in-sample): %.6f" % metrics.mean_squared_error(y_train, y_pred))
-
-    def get_prediction(self):
-        """
-        Get signal value
-        """
-        logging.info("built signals from %s to %s" % (self.start, self.end))
-
-        # shift y
-        af = self.alpha_source.copy()
-        af['ret1d'] = af.groupby('symbol')['ret'].shift(-1).fillna(0)
-
-        # split train and test
-        x, y = af.drop(columns=['ret1d']), af['ret1d']
-        x_test = x.loc[(x.index.get_level_values(0) >= self.test_start) & (x.index.get_level_values(0) < self.test_end)]
-        y_true = y.loc[(y.index.get_level_values(0) >= self.test_start) & (y.index.get_level_values(0) < self.test_end)]
-        y_pred = self.model.predict(x_test)
-
-        # print out-of-sample evaluation
-        print("Mean absolute error (out-of-sample): %.6f" % metrics.mean_absolute_error(y_true, y_pred))
-        print("Mean squared error (out-of-sample): %.6f" % metrics.mean_squared_error(y_true, y_pred))
-
-        # FIXME: fill zero is too brute-force
-        self.returns = pd.Series(index=x_test.index, data=y_pred).unstack().shift(1).fillna(0)
-
-        # construct outputs
-        self.returns['cash'] = 0
-        return self.returns
+        return train_result, test_result
